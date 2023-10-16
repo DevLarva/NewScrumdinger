e12e93583fe40ecf1d06da97c68041e6132929aa
백대홍 <bdh3620@naver.com>
2023-10-16 23:49:58 +0900

    FEAT: 3.1 Navigation and modal presentation
    Creating a navigation hierarchy

---
+++ NewScrumdinger/DetailView.swift
@@ -0,0 +1,55 @@
+//
+//  DetailView.swift
+//  NewScrumdinger
+//
+//  Created by 백대홍 on 10/16/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .ignore)
+                
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.rawValue)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                    
+                }
+                .accessibilityElement(children: .ignore)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
+        .navigationTitle(scrum.title)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
--- NewScrumdinger/Models/DailyScrum.swift
+++ NewScrumdinger/Models/DailyScrum.swift
@@ -11,20 +11,30 @@ import Foundation
 struct DailyScrum: Identifiable {  //구조체 생성
     let id: UUID
     var title: String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthInMinutes: Int
     var theme: Theme
     
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {   //id속성에 기본값을 할당하는 초기화 작업
-          self.id = id
-          self.title = title
-          self.attendees = attendees
-          self.lengthInMinutes = lengthInMinutes
-          self.theme = theme
-      }
+        self.id = id
+        self.title = title
+        self.attendees = attendees.map { Attendee(name: $0) }
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
-
+extension DailyScrum {
+    struct Attendee: Identifiable {
+        let id: UUID
+        var name: String
+        
+        init(id: UUID = UUID(), name: String) { //초기화 작업
+            self.id = id
+            self.name = name
+        }
+    }
+}
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
